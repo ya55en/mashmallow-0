281b3f8472bd21e84782a0e1eaceea476aa98697
Philip Damyanov <fd@itlabs.bg>
2021-09-22 10:46:37 +0300

    Complete Wire install script #14

--- src/share/recipes/install/wire.sh
+++ src/share/recipes/install/wire.sh
@@ -8,42 +8,116 @@ DEBUG=true
 _ARCH=x86_64
 _LOCAL="$HOME/.local"
 
+# Install Wire
 version='3.26.2941'
+app_file="Wire-${version}_${_ARCH}.AppImage"
+app_fullpath="$_LOCAL/opt/wire/${app_file}"
+download_target="${_DOWNLOAD_CACHE}/${app_file}"
+icon_file="wire-icon-dark-128.png"
+mkdir -p "${_DOWNLOAD_CACHE}"
 
 log debug "version=[$version]"
 
-_URL_DOWNLOAD="https://wire-app.wire.com/linux/Wire-${version}_${_ARCH}.AppImage"
+# https://wire-app.wire.com/linux/Wire-3.26.2941_x86_64.AppImage
+_URL_DOWNLOAD="https://wire-app.wire.com/linux/${app_file}"
 
 log debug "_URL_DOWNLOAD=[$_URL_DOWNLOAD]"
 
+download_appimage() {
+  #: Download and install the app image
 
-# Download and install:
-log info "Downloading Wire desktop v${version}..."
-mkdir -p "$_LOCAL/opt/wire"
-curl -sL "$_URL_DOWNLOAD" -o $_LOCAL/opt/wire/Wire-${version}-${_ARCH}.AppImage__OFF
+    log debug "download_target=[$download_target]"
+    if [ -e "${download_target}" ]; then
+        log warn "App file already downloaded/cached, skipping."
+    else
+        log info "Downloading Wire desktop v${version}..."
+        curl -sL "$_URL_DOWNLOAD" -o "${download_target}"
+    fi
+}
 
+check_hashsum() {
+    #: Check the app image file hashsum.
 
-# TODO: check sha512
+    # TODO: check sha512
+    true
+}
 
-mv -f $_LOCAL/opt/wire/Wire-${version}-${_ARCH}.AppImage__OFF $_LOCAL/opt/wire/Wire-${version}-${_ARCH}.AppImage
-chmod +x $_LOCAL/opt/wire/Wire-${version}-${_ARCH}.AppImage
-ln -fs $_LOCAL/opt/wire/Wire-${version}-${_ARCH}.AppImage $_LOCAL/bin/wire-desktop
+install_app_image() {
+    if [ -e "${app_fullpath}" ]; then
+        log warn "Installed app file already exists, skipping. (${app_fullpath})"
+    else
+        log info "Installing app file ... (${app_fullpath})"
+        mkdir -p "$_LOCAL/opt/wire"
+        cp -p "${download_target}" "${app_fullpath}"
+        chmod +x "${app_fullpath}"
+        ln -fs "${app_fullpath}" "$_LOCAL/bin/wire-desktop"
+    fi
+}
 
-cat <<EOS
+setup_gnome_assets() {
+    #: Create/copy .desktop file and an icon.
 
-In order to have all your terminals know about the add-to-path change,
-you need to:
-  - EITHER source the add-top-path script (see below) in all open terminals,
-  - OR log out, then log back in.
+    [ -e "${_LOCAL}/share/applications/com.wire.desktop" ] && [ -e "${_LOCAL}/opt/wire/${icon_file}" ]
 
-To source the add-to-path script, do (note the dot in front):
+    if [ $? = 0 ]; then
+        log info "Gnome assets already installed, skipping."
+    else
+        log info "Installing gnome assets ..."
+        # shellcheck disable=SC1090
+        . "$_APPLICATIONS_DIR/com.wire.desktop" > "${_LOCAL}/share/applications/com.wire.desktop"
+        cp -p "${_ICONS_DIR}/${icon_file}" "${_LOCAL}/opt/wire/${icon_file}"
+    fi
+}
 
- $ . "~/.bashrc.d/42-pycharm-${flavor}.sh"
+inform_user() {
+    cat <<EOS
 
- Pycharm should be accessible now from anywhere on the command line,
- with:
+Completed. You can use Wire Desktop App instantly, like:
 
-  $ pycharm.sh
+  $ wire-desktop
+
+Enjoy! ;)
 
 EOS
-log info 'Done! ;)'
+}
+
+doit() {
+    log info "** Installing Wire:"
+    download_appimage
+    check_hashsum
+    install_app_image
+    setup_gnome_assets
+    inform_user
+}
+
+undo() {
+    log info "** Un-installing Wire:"
+
+    result=''
+
+    # Remove gnome assets
+    log info "Removing gnome assets..."
+    rm -f "${_LOCAL}/opt/wire/wire-icon.png"
+    result="${result}:$?"
+    rm -f "${_LOCAL}/share/applications/com.wire.desktop"
+    result="${result}:$?"
+
+    # Remove symlink
+    log info "Removing symlink..."
+    rm -f "$_LOCAL/bin/wire-desktop"
+    result="${result}:$?"
+
+    # Remove app image file
+    log info "Removing wire app image..."
+    rm -f "${app_fullpath}"
+    result="${result}:$?"
+
+    if [ x"$result" = 'x:0:0:0:0' ]; then
+        log info "Successfully un-installed wire."
+    else
+        log warn "Un-installed wire with errors: codes [${result}]"
+    fi
+}
+
+doit
+#undo
