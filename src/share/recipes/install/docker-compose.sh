d9f179d0c8923fae5b80f381762d23b29b84def6
Yassen Damyanov <yd@itlabs.bg>
2021-09-29 15:45:28 +0300

    Improve docker + docker-compose recpies #38

--- src/share/recipes/install/docker-compose.sh
+++ src/share/recipes/install/docker-compose.sh
@@ -1,30 +1,35 @@
 #! /bin/sh
 
+DEBUG=true
+
+_ARCH="$(uname -m)"
+_KERNEL="$(uname -s)"
+# defined in etc/mashrc  # TODO: remove
+#_LOCAL="$HOME/.local"
+
+_URL_LATEST='https://github.com/bitwarden/desktop/releases/latest'
+_URL_DOWNLOAD_RE='^location: https://github.com/docker/compose/releases/tag/v\(.*\)$'
+#_VERSION="$(curl -Is $_URL_LATEST | grep '^location' | tr -d '\n\r' | sed "s|$_URL_DOWNLOAD_RE|\1|")"
+_VERSION='1.29.2'
+
+# TODO: Versions 2.x follow different URL/ARCH naming convention and need different handling
+
+_BIN_LOC="${_LOCAL}/bin/docker-compose"
+_FILE_URL="https://github.com/docker/compose/releases/download/${_VERSION}/docker-compose-${_KERNEL}-${_ARCH}"
+
+log debug "_VERSION=$_VERSION"
+log debug "_FILE_URL=$_FILE_URL"
+
 doit() {
-    echo x"$(sudo whoami)"
-    if [ x"$(sudo whoami)" != xroot ]; then
-        die 15 'FATAL: Not sudo root, terminating'
-    fi
-
-    if [ x"$HOME" == xroot ]; then
-        die 15 'FATAL: Root but not sudo root, terminating'
-    fi
-
-    if curl -V > /dev/null 2>&1; then :; else
-        die 15 'FATAL: curl not found, terminating'
-    fi
-
-    # TODO: take the latest version automagically
-
-    _ARCH=x86_64
-    _LOCAL="$HOME/.local"
-    _VERSION='1.27.4'
-    _BIN_LOC="$HOME/.local/bin/docker-compose"
-    _FILE_URL="https://github.com/docker/compose/releases/download/${_VERSION}/docker-compose-$(uname -s)-$(uname -m)"
-
-    curl -L "${_FILE_URL}" -o "${_BIN_LOC}"
+    curl -sL "${_FILE_URL}" -o "${_BIN_LOC}"
     chmod +x "${_BIN_LOC}"
-    docker-compose --version && echo 'Done.' || die 12 'Docker-compose installation FAILED'
+    docker-compose --version > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        log info 'docker-compose installation successful.'
+        docker-compose --version
+    else
+        log error 'docker-compose installation FAILED.'
+    fi
 }
 
 undo() {
--- src/share/recipes/install/docker.sh
+++ src/share/recipes/install/docker.sh
@@ -4,23 +4,22 @@
 _USER="$USER"
 
 # TODO: auto-detect distro name, etc.
-_DISTRO=focal
-
-printf "Working as user '%s'...\n" "$_USER"
+_CODENAME=focal
+_DISTRO=ubuntu
 
 already_has_docker() {
     #: Return true (0) if docker has been installed, false otherwise.
     # TODO: Check docs for 'systemctl status' to make sure this impl is correct.
 
-    systemctl status dockAr.service --no-pager
-    [ $? -lt 4 ]
+    systemctl status docker.service --no-pager
+    test $? -lt 4
 }
 
 ensure_proper_distro() {
     #: Fail if not on Ubuntu (currently nothing else is supported).
 
-    if ! uname -v | grep -iq ubuntu; then
-        die 15 'FATAL: Not on ubuntu, terminating'
+    if ! uname -v | grep -iq "${_DISTRO}"; then
+        die 15 'Not on ${_DISTRO}, terminating'
     fi
 }
 
@@ -28,7 +27,7 @@ ensure_sudoer() {
     #: Fail if current user is not a sudoer.
 
     if ! /bin/sudo apt --version; then
-        die 14 'NOT a sudo user; terminaing.'
+        die 14 'NOT a sudo user; terminating.'
     fi
 }
 
@@ -47,47 +46,74 @@ setup_apt_docker_repo() {
     sudo apt-get update
     sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu ${_DISTRO} stable"
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu ${_CODENAME} stable"
 }
 
 install_docker_ce() {
     #: Install docker from specialized apt repository. Assumes the repository
     #: has already been set up.
-    # setup_apt_repo || die 14 'add-apt-repository FAILED'
 
-    if ! sudo apt-get update && sudo apt install -y docker-ce; then
-        die 12 'apt install docker-ce FAILED'
-    fi
+    sudo apt-get update || die 14 'sudo apt-get update FAILED!'
+    sudo apt install -y docker-ce || die 14 'sudo apt install docker-ce FAILED!'
 }
 
-enable_user_to_run_docker() {
-    #: Add current user to grop docker providing access to the docker
+add_user_to_group_docker() {
+    #: Add current user to group docker providing access to the docker
     #: daemon unix socket.
+
+    log info "Adding user '${_USER}' to group 'docker'..."
     sudo usermod -aG docker "${_USER}"
+}
+
+disable_docker_service() {
+    log info "Disabling docker service..."
+    log info " (docker will still work fine when invoked on the cmd line.)"
     sudo systemctl disable docker
     systemctl status docker --no-pager
 }
 
 smoke_test() {
     #: Smoke-test current user's ability to run docker images.
+
     if docker run hello-world; then
-        printf '\nSUCESS.'
+        printf '\nSUCESS.\n'
     else
-        die 12 '\nDocker installation has FAILED.'
+        die 12 '\nDocker smoke text FAILED.'
     fi
 }
 
 explain_logout_need() {
     #: Explain the need to log out and then log back in to have
     #: the group addition take effect.
+
     cat << EOS
 
 Your user '${_USER}' has been added to group 'docker' so that
 you can use docker without becoming a super-user.
 
-You need to log out and then log back in so that this may take
-effect. After logging out and back in, re-run this script to do
-final checks.
+You need to **REBOOT** so that this may take effect.
+
+After logging out and back in, please **re-RUN this command** [1]
+to do final checks.
+
+Cheers! ;)
+
+______
+[1] Re-run 'mash install docker' again after logging back in
+
+EOS
+}
+
+inform_docker_is_working() {
+    #: Explain docker has already been installed (method unknown)
+    #: and working.
+
+    cat << EOS
+
+Docker has been found to be installed and working:
+
+ \$ docker --version
+ $(docker --version)
 
 Cheers! ;)
 
@@ -96,17 +122,30 @@ EOS
 
 doit() {
     if already_has_docker; then
-        systemctl restart docker.service
-        smoke_test
+        sudo systemctl restart docker.service
+        smoke_test || die 14 'docker already installed but not working (?)'
+        inform_docker_is_working
     else
         ensure_sudoer
         ensure_proper_distro
         ensure_curl_present
+        log info "Installing docker on ${_DISTRO} ${_CODENAME}..."
         setup_apt_docker_repo
         install_docker_ce
+        add_user_to_group_docker
+        disable_docker_service
+        sudo systemctl restart docker.service
         explain_logout_need
     fi
+}
 
+undo() {
+    cat << EOS
+
+'undo install docker' not yet implemented. We are sorry
+for the inconvenience.
+
+EOS
 }
 
 $mash_action
