3f75c7cff38053389bd6ec6e337a217b81199f13
Veren Damyanov <veren99@gmail.com>
2021-12-18 12:16:46 +0000

    Remove libraries and redirect references to sh-stdlib #70

--- scripts/4make/setenv.sh
+++ scripts/4make/setenv.sh
@@ -5,7 +5,9 @@ NEW_MASH_HOME="$(pwd)/src"
 cat << EOS
 export MASH_HOME=$NEW_MASH_HOME ; \
   export PATH="$NEW_MASH_HOME/bin:\$PATH" ; \
-  export MASH_IMPORT_PATH="$NEW_MASH_HOME/lib:$NEW_MASH_HOME/etc:$NEW_MASH_HOME/../chroot-setup/scripts" ; \
+  export POSIXSH_IMPORT_PATH="$NEW_MASH_HOME/lib:$NEW_MASH_HOME/etc:$POSIXSH_IMPORT_PATH"; \
   echo "MASH_HOME=\$MASH_HOME" ; \
   echo "PATH=\$PATH"
 EOS
+
+# export MASH_IMPORT_PATH="$NEW_MASH_HOME/lib:$NEW_MASH_HOME/etc:$NEW_MASH_HOME/../chroot-setup/scripts" ; \
