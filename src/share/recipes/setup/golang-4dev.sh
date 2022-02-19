61db8487343bd2c4ab83d64801e9ffa33495e639
Veren Damyanov <veren.damyanov@protonmail.com>
2022-02-19 17:44:31 +0000

    Implement install_multi method in install library and rework golang recipe to use it #80

--- src/share/recipes/setup/golang-4dev.sh
+++ src/share/recipes/setup/golang-4dev.sh
@@ -5,6 +5,7 @@
 
 import logging
 import os
+import install
 
 #: Extract the go version out of the first five lines of the JSON
 #: response to the given HTTP GET request.
@@ -34,58 +35,55 @@ download_tarball() {
     fi
 }
 
-extract_tarball() {
-    local target_dir="$_LOCAL/opt/$opt_dir"
-
-    if [ -e "$target_dir/$version" ]; then
-        _warn "Current version seems to have been installed in $target_dir/$version"
-    else
-        mkdir -p "$target_dir"
-        _info "Extracting tarball into $target_dir ..."
-        tar xf "$_DOWNLOAD_CACHE/$archive_filename" -C "$target_dir"
-        mv "$target_dir/go" "$target_dir/${version}"
-    fi
-}
-
-create_symlink() {
-    local target_dir="$_LOCAL/opt/$opt_dir"
-    if [ -L "$target_dir/current" ]; then
-        _warn "Symlink $target_dir/current has already been created, skipping."
-    else
-        _info "Creating symlink 'current' to $target_dir/$version ..."
-        into_dir_do "$target_dir" "ln -s $version current"
-        [ -e "$target_dir/$version" ] || {
-            die 33 "Creating symlink 'current' to $target_dir/$version FAILED"
-        }
-    fi
-}
-
-#create_bashrcd_script() {
-#    _info "create_bashrcd_script..."
+#extract_tarball() {
+#    local target_dir="$_LOCAL/opt/$opt_dir"
+#
+#    if [ -e "$target_dir/$version" ]; then
+#        _warn "Current version seems to have been installed in $target_dir/$version"
+#    else
+#        mkdir -p "$target_dir"
+#        _info "Extracting tarball into $target_dir ..."
+#        tar xf "$_DOWNLOAD_CACHE/$archive_filename" -C "$target_dir"
+#        mv "$target_dir/go" "$target_dir/${version}"
+#    fi
 #}
-#: Create environment setup script in ~/.bashrc.d/
-create_bashrcd_script() {
-    local linked_dir="$_LOCAL/opt/${opt_dir}/current"
-
-    env_script_fullpath="$HOME/.bashrc.d/88-golang-path.sh"
-    if [ -e "$env_script_fullpath" ]; then
-        _warn "Env setup script for golang already exists, skipping ($env_script_fullpath)"
-        return 4
-    fi
-
-    _info "Creating env setup script ($env_script_fullpath) ..."
-    cat > "$env_script_fullpath" << EOS
-# $env_script_fullpath - mash: add golang bin to PATH
-
-_GO_HOME='${linked_dir}'
-echo \$PATH | grep -q "\$_GO_HOME/bin" || PATH="\$_GO_HOME/bin:\$PATH"
-
-EOS
-}
+#
+#create_symlink() {
+#    local target_dir="$_LOCAL/opt/$opt_dir"
+#    if [ -L "$target_dir/current" ]; then
+#        _warn "Symlink $target_dir/current has already been created, skipping."
+#    else
+#        _info "Creating symlink 'current' to $target_dir/$version ..."
+#        into_dir_do "$target_dir" "ln -s $version current"
+#        [ -e "$target_dir/$version" ] || {
+#            die 33 "Creating symlink 'current' to $target_dir/$version FAILED"
+#        }
+#    fi
+#}
+#
+##: Create environment setup script in ~/.bashrc.d/
+#create_bashrcd_script() {
+#    local linked_dir="$_LOCAL/opt/${opt_dir}/current"
+#
+#    env_script_fullpath="$HOME/.bashrc.d/88-golang-path.sh"
+#    if [ -e "$env_script_fullpath" ]; then
+#        _warn "Env setup script for golang already exists, skipping ($env_script_fullpath)"
+#        return 4
+#    fi
+#
+#    _info "Creating env setup script ($env_script_fullpath) ..."
+#    cat > "$env_script_fullpath" << EOS
+## $env_script_fullpath - mash: add golang bin to PATH
+#
+#_GO_HOME='${linked_dir}'
+#echo \$PATH | grep -q "\$_GO_HOME/bin" || PATH="\$_GO_HOME/bin:\$PATH"
+#
+#EOS
+#}
 
 smoke_test() {
     _info "Smoke-testing the installation..."
-    . "$env_script_fullpath"
+    . "$HOME/.bashrc.d/88-golang-path.sh"
 
     if go version; then
         _info "Smoke test passed."
@@ -116,16 +114,17 @@ doit() {
     local archive_ext='tar.gz'
     local archive_filename
     local opt_dir='golang'
-    local env_script_fullpath
+    local env_filename='88-golang-path.sh'
 
     get_latest_version
     archive_basename="go${version}.linux-${_OS_ARCH_SHORT}"
     archive_filename="$archive_basename.$archive_ext"
 
     download_tarball
-    extract_tarball
-    create_symlink
-    create_bashrcd_script
+    install_multi "$_DOWNLOAD_CACHE/$archive_filename" 'golang' "$version" "$env_filename"
+#    extract_tarball
+#    create_symlink
+#    create_bashrcd_script
     smoke_test
     inform_user
 }
