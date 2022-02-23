16bb9fe5efc83fb7880d5a79296410f3449ada2c
Veren Damyanov <veren.damyanov@protonmail.com>
2022-02-23 19:51:45 +0000

    Change install_multi to separate bashrcd script setup and rework nodejs recipe install

--- src/lib/install.sh
+++ src/lib/install.sh
@@ -27,36 +27,44 @@ install_multi() {
     #:    ($1) tarball_path - path to the source tarball
     #:    ($2) recipe_name - the name of the recipe currently being installed
     #:    ($3) version - the version of the recipe currently being installed
-    #:    ($4) env_filename - the name for the ~/.bashrc.d/ script file
-    local tarball_path="$1"; local recipe_name="$2"; local version="$3"; local env_filename="$4"
+    local tarball_path="$1"; local recipe_name="$2"; local version="$3"
 
     #: create proper .local/opt/name/version directory structure and extract the tarball
     local recipe_dir="$_LOCAL/opt/$recipe_name"
-    if [ -e "$recipe_dir" ]; then
+    if [ -e "$recipe_dir/$version" ]; then
         _warn "Current version seems to have been installed in $recipe_dir"
-    else
-    mkdir -p "$recipe_dir"
-        _info "Extracting tarball into $recipe_dir ..."
-        tar xf "$tarball_path" -C "$recipe_dir"
-        mv "$recipe_dir/$(ls $recipe_dir)" "$recipe_dir/$version"
+        # exit here?
     fi
-    #: create "current" symlink
+    mkdir -p "$recipe_dir"
+    _info "Extracting tarball into $recipe_dir ..."
+    tar xf "$tarball_path" -C "$recipe_dir" ||
+        die $? "Extracting $source_path FAILED (rc=$?)"
+    mv "$recipe_dir/$(ls $recipe_dir)" "$recipe_dir/$version"
+    #: create 'current' symlink
     if [ -L "$recipe_dir/current" ]; then
         _warn "Symlink $recipe_dir/current has already been created, skipping."
     else
         _info "Creating symlink 'current' to $recipe_dir/$version ..."
         ln -fs  "$recipe_dir/$version" "$recipe_dir/current"
-        [ -e "$recipe_dir/$version" ] || {
-            die 33 "Creating symlink 'current' to $recipe_dir/$version FAILED"
-        }
+        [ -e "$recipe_dir/$version" ] || die 33 "Creating symlink 'current' to $recipe_dir/$version FAILED"
     fi
-    #: create ~/.bashrc.d/ script
+
+}
+
+install_bashrcd_script(){
+    #: Used to create ~/.bashrc.d/ script to setup path
+    #:    ($1) recipe_name - the name of the recipe currently being installed
+    #:    ($2) env_filename - the name for the ~/.bashrc.d/ script file
+    local recipe_name="$1"; local env_filename="$2"
+
     local env_path="$HOME/.bashrc.d/$env_filename"
+    local recipe_dir="$_LOCAL/opt/$recipe_name"
     if [ -e "$env_path" ]; then
         _warn "Env setup script for $recipe_name already exists, skipping ($env_path)"
         return 4
     fi
     _info "Creating env setup script ($env_path) ..."
+
     cat > "$env_path" << EOS
 # $env_path - mash: add ${recipe_name} bin to PATH
 
@@ -65,6 +73,7 @@ echo \$PATH | grep -q "\$_RECIPE_HOME/bin" || PATH="\$_RECIPE_HOME/bin:\$PATH"
 
 EOS
 }
+
 
 
 if [ "$_name_" = "$_install_name_" ]; then
